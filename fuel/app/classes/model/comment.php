9150a3fbb530f902730ebe474ff511f9a309152b
sskre <sskre@users.noreply.github.com>
2017-05-19 00:31:25 +0700

    Modified to be use Observer_Validation for Comment model

--- fuel/app/classes/model/comment.php
+++ fuel/app/classes/model/comment.php
@@ -3,48 +3,75 @@ use Orm\Model_Soft;
 
 class Model_Comment extends Model_Soft
 {
-	protected static $_properties = array(
+	protected static $_properties = [
 		'id',
-		'article_id',
-		'author',
-		'contents',
-		'created_at',
-		'updated_at',
-		'deleted_at',
-	);
+		'article_id' => [
+			'data_type' => 'int',
+			'form' => [
+				'type' => false,
+			],
+		],
+		'author' => [
+			'data_type' => 'text',
+			'label' => 'Author',
+			'validation' => ['required'],
+			'form' => [
+				'type' => 'text',
+			],
+		],
+		'contents' => [
+			'data_type' => 'text',
+			'label' => 'Contents',
+			'validation' => ['required'],
+			'form' => [
+				'type' => 'text',
+			],
+		],
+		'created_at' => [
+			'data_type' => 'datetime',
+			'form' => [
+				'type' => false,
+			],
+		],
+		'updated_at' => [
+			'data_type' => 'datetime',
+			'form' => [
+				'type' => false,
+			],
+		],
+		'deleted_at' => [
+			'data_type' => 'datetime',
+			'form' => [
+				'type' => false,
+			],
+		],
+	];
 
-	protected static $_observers = array(
-		'Orm\Observer_CreatedAt' => array(
-			'events' => array('before_insert'),
+	protected static $_observers = [
+		'Orm\Observer_CreatedAt' => [
+			'events' => ['before_insert'],
 			'mysql_timestamp' => true,
-		),
-		'Orm\Observer_UpdatedAt' => array(
-			'events' => array('before_save'),
+		],
+		'Orm\Observer_UpdatedAt' => [
+			'events' => ['before_save'],
 			'mysql_timestamp' => true,
-		),
-	);
+		],
+		'Orm\Observer_Validation' => [
+			'events' => ['before_save'],
+		],
+	];
 
-	protected static $_soft_delete = array(
+	protected static $_soft_delete = [
 		'mysql_timestamp' => true,
-	);
+	];
 
-	protected static $_belongs_to = array(
-		'article' => array(
+	protected static $_belongs_to = [
+		'article' => [
 			'key_from' => 'article_id',
 			'model_to' => 'Model_Article',
 			'key_to' => 'id',
 			'cascade_save' => false,
 			'cascade_delete' => false,
-		),
-	);
-
-	public static function validate($factory)
-	{
-		$val = Validation::forge($factory);
-		$val->add_field('author', 'Author', 'required');
-		$val->add_field('contents', 'Contents', 'required');
-
-		return $val;
-	}
-
+		],
+	];
 }
